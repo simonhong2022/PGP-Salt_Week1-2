5d141155205b5597ffecaf947024c2cda7caa56f
simon <simonhong85@gmail.com>
2023-05-10 13:17:35 +0200

    populate json data

--- addons-ts-lab-react-fullstack-main/addons-ts-lab-react-fullstack-main/puppies/src/pages/api/puppies.ts
+++ addons-ts-lab-react-fullstack-main/addons-ts-lab-react-fullstack-main/puppies/src/pages/api/puppies.ts
@@ -1,9 +1,9 @@
 import { Dispatch, SetStateAction } from "react";
 
 export interface IPuppy {
-     breed: string,
-     name: string,
-     birthdate: string
+    breed: string,
+    name: string,
+    birthdate: string
 }
 
 const BASE_PATH: string = 'http://localhost:9000/api/puppies';
@@ -11,7 +11,81 @@ const BASE_PATH: string = 'http://localhost:9000/api/puppies';
 export async function fetchPuppies(setPuppies: Dispatch<SetStateAction<IPuppy[]>>) {
     const response = await fetch(BASE_PATH);
     const responseData: IPuppy[] = await response.json();
-    
+
     setPuppies(responseData);
+
+}
+
+export async function addPuppy(event: React.FormEvent<HTMLFormElement>,
+    setPuppies: Dispatch<SetStateAction<IPuppy[]>>, setOpen: Dispatch<SetStateAction<boolean>>,
+    setErrMessage: Dispatch<SetStateAction<string>>) {
+    const reqBody: IPuppy = {
+        breed: event.currentTarget.breed.value,
+        name: event.currentTarget.pname.value,
+        birthdate: event.currentTarget.birthdate.value
+    };
+
+    if (!reqBody.breed) {
+        setErrMessage('Please choose a breed.');
+        return;
+    }
+
+    if (!reqBody.name) {
+        setErrMessage('Please choose a name.');
+        return;
+    }
+
+    const reqOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody)
+    };
+
+    const response = await fetch(BASE_PATH, reqOptions);
+    await fetchPuppies(setPuppies);
+    setOpen(false);
+    setErrMessage('');
+
+}
+
+export async function updatePuppy(id: string, event: React.FormEvent<HTMLFormElement>,
+     setPuppies: Dispatch<SetStateAction<IPuppy[]>>,setOpen: Dispatch<SetStateAction<boolean>>,
+     setErrMessage: Dispatch<SetStateAction<string>> ) {
+     
+     const reqBody: IPuppy = {
+        breed: event.currentTarget.breed.value,
+        name: event.currentTarget.pname.value,
+        birthdate: event.currentTarget.birthdate.value
+     }
+
+     if (!reqBody.breed) {
+        setErrMessage('Please choose a breed.');
+        return;
+    }
+
+    if (!reqBody.name) {
+        setErrMessage('Please choose a name.');
+        return;
+    }
+
+    const reqOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody)
+    };
+
+    const response = await fetch(BASE_PATH, reqOptions);
+    await fetchPuppies(setPuppies);
+    setOpen(false);
+    setErrMessage('');
+    
+}
+
+export async function deletePuppy(id: string, setPuppies: Dispatch<SetStateAction<IPuppy[]>>) {
+    const reqOptions = {
+        method: 'DELETE'
+    }
+    const response = await fetch(`${BASE_PATH}/${id}`, reqOptions);
+    await fetchPuppies(setPuppies);
 
 }
