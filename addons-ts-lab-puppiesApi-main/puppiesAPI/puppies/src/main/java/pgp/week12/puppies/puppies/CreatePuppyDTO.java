905d61992c81896230b73139d3b2c0a8cbc1500f
simon <simonhong85@gmail.com>
2023-05-15 11:32:23 +0200

    fix jsonproperty

--- addons-ts-lab-puppiesApi-main/puppiesAPI/puppies/src/main/java/pgp/week12/puppies/puppies/CreatePuppyDTO.java
+++ addons-ts-lab-puppiesApi-main/puppiesAPI/puppies/src/main/java/pgp/week12/puppies/puppies/CreatePuppyDTO.java
@@ -1,6 +1,7 @@
 package pgp.week12.puppies.puppies;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record CreatePuppyDTO(@JsonProperty String id, @JsonProperty String breed,
-                             @JsonProperty String name, @JsonProperty String birthdate) {
+public record CreatePuppyDTO(@JsonProperty("id") String id, @JsonProperty("breed") String breed,
+                             @JsonProperty("name") String name, @JsonProperty("birthdate") String birthdate) {
+
 }
